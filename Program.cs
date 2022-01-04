e948b98593a0286378bf4c28c08a7421397a587d
FanuelConrad <59276995+FanuelConrad@users.noreply.github.com>
2022-01-04 10:41:28 +0300

    not yet complete

--- Program.cs
+++ Program.cs
@@ -1,8 +1,13 @@
-﻿using System;
+﻿using Microsoft.ML;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+
+string _dataPath = Path.Combine(Environment.CurrentDirectory, "Data", "iris.data");
+string _modelPath = Path.Combine(Environment.CurrentDirectory, "Data", "IrisClusteringModel.zip");
 
 namespace IrisFlowerClustering
 {
@@ -10,6 +15,10 @@ namespace IrisFlowerClustering
     {
         static void Main(string[] args)
         {
+            var mlContext = new MLContext(seed: 0);
+
+            //load data to the dataview
+            IDataView dataView = mlContext.Data.LoadFromTextFile<IrisData>(_dataPath, hasHeader: false, separatorChar: ',');
         }
     }
 }
